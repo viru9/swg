9fecd92e00711a6977a44ca4509327abf97d4e16
Viraj <viraj@zmessenger.mobi>
2020-11-04 04:06:41 +0530

    changes added

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import {AUTH_URL, CLIENT, LANDING_PAGE, REDIRECT_URL, TOKEN_NAME} from "../../config/config"
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Box from '@material-ui/core/Box';
+import './login.css';
 
 class Login extends Component {
 
@@ -12,6 +18,17 @@ class Login extends Component {
             return true;
         return false
     }
+
+    Copyright = () => {
+        return (
+          <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+              viraj
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        );
+      }
 
     componentDidMount() {
 
@@ -27,15 +44,25 @@ class Login extends Component {
     }
 
     render(){
+       
         return(
-            <Container maxWidth="sm">
-                <Button onClick={() => {
+            <Container component="main" maxWidth="xs">
+                <div className={"paper"}>
+                <CssBaseline />
+                <Avatar className={"avatar"}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Button fullWidth onClick={() => {
                     let url = AUTH_URL + '?client_id=' + CLIENT + '&redirect_uri=' + REDIRECT_URL;
                     window.location.assign(url);
                 }}
                   variant="contained" color="primary" >
-                    Login
+                    Sign in
                 </Button>
+                <Box mt={8}>
+                    {this.Copyright()}
+                </Box>
+                </div>
             </Container>
             
             )
